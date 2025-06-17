c81c7893407316276e3f81de817281328bbae67a
Mariano <mariano_ef@live.com>
2025-06-17 00:32:07 -0300

    stores procedures (prefe 1, noti 1, prefe 2, noti 2) + fixes noticia

--- scripts.sql
+++ scripts.sql
@@ -57,7 +57,8 @@ CREATE TABLE noticia (
    fecha_publicacion DATETIME DEFAULT GETDATE(),
    portal_id INT FOREIGN KEY REFERENCES portales_noticia(id),
    tematica_id INT FOREIGN KEY REFERENCES tematica_noticias(id),
-   autor NVARCHAR(100)
+   autor NVARCHAR(100),
+    url_original nvarchar(max)
 );
 GO
 
@@ -96,3 +97,100 @@ CREATE TABLE portales_bloq (
     bloq BIT NOT NULL DEFAULT 1
 );
 GO;
+
+create procedure preferencias_tot_usuario
+    @id_usuario int
+as
+    begin
+        SELECT
+            tn.id,
+            tn.nombre,
+            IIF(pnu.interesa = 1, 1, 0) AS interesa
+        FROM tematica_noticias tn
+        LEFT JOIN preferencia_noticias_usuario pnu
+            ON tn.id = pnu.tematica_id AND pnu.usuario_id = @id_usuario;
+    end;
+GO;
+
+create procedure portales_total_bloq
+    @usuario_id int
+    as
+        select
+            portales_noticia.id,
+            nombre,
+            logo,
+            url_base,
+            iif(bloq = 0 or bloq is null, 0, 1) as bloqueado
+        from portales_noticia
+        left join portales_bloq
+            on portales_noticia.id = portales_bloq.portal_id
+            and portales_bloq.usuario_id = @usuario_id;
+GO;
+
+create procedure obtener_noticias_usuario
+    @usuario_id int
+    as
+SELECT
+    noticia.id,
+    noticia.titulo,
+    noticia.resumen,
+    noticia.fecha_publicacion,
+    noticia.url_original,
+    noticia.portal_id,
+    portales_noticia.nombre as portal_nombre,
+    tematica_noticias.id as tematica_id,
+    tematica_noticias.nombre as tematica_nombre,
+    noticia.autor,
+    interaccion_noticia_usuario.id as interaccion_id,
+    interaccion_noticia_usuario.fecha_leido,
+    interaccion_noticia_usuario.utilidad,
+    interaccion_noticia_usuario.resumen_claro
+FROM noticia
+LEFT JOIN interaccion_noticia_usuario
+  ON noticia.id = interaccion_noticia_usuario.noticia_id
+  AND interaccion_noticia_usuario.usuario_id = @usuario_id
+INNER JOIN portales_noticia
+  ON noticia.portal_id = portales_noticia.id
+INNER JOIN tematica_noticias
+  ON noticia.tematica_id = tematica_noticias.id
+INNER JOIN preferencia_noticias_usuario
+  ON preferencia_noticias_usuario.usuario_id = @usuario_id
+  AND preferencia_noticias_usuario.tematica_id = noticia.tematica_id
+LEFT JOIN portales_bloq
+  ON portales_bloq.portal_id = noticia.portal_id
+  AND portales_bloq.usuario_id = @usuario_id
+WHERE (portales_bloq.portal_id IS NULL or portales_bloq.bloq = 0)
+  AND noticia.resumen IS NOT NULL
+  AND preferencia_noticias_usuario.interesa = @usuario_id
+order by noticia.fecha_publicacion desc;
+go;
+
+create procedure get_noticia_usuario
+    @usuario_id int,
+    @noticia_id int
+as
+SELECT
+    noticia.id,
+    noticia.titulo,
+    noticia.resumen,
+    noticia.fecha_publicacion,
+    noticia.url_original,
+    noticia.portal_id,
+    portales_noticia.nombre as portal_nombre,
+    tematica_noticias.id as tematica_id,
+    tematica_noticias.nombre as tematica_nombre,
+    noticia.autor,
+    interaccion_noticia_usuario.id as interaccion_id,
+    interaccion_noticia_usuario.fecha_leido,
+    interaccion_noticia_usuario.utilidad,
+    interaccion_noticia_usuario.resumen_claro
+FROM noticia
+INNER JOIN tematica_noticias
+    ON noticia.tematica_id = tematica_noticias.id
+INNER JOIN portales_noticia
+    ON noticia.portal_id = portales_noticia.id
+LEFT JOIN interaccion_noticia_usuario
+    ON noticia.id = interaccion_noticia_usuario.noticia_id
+   AND interaccion_noticia_usuario.usuario_id = @usuario_id
+WHERE noticia.id = @noticia_id;
+go;
